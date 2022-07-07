15b8162fb185d4738a5e038e468d0a69fd6f23c1
cchoijjinyoung <cgy7127@naver.com>
2022-07-07 11:13:10 +0900

    Update BoardControllerTest

--- src/test/java/com/devil/api/controller/BoardControllerTest.java
+++ src/test/java/com/devil/api/controller/BoardControllerTest.java
@@ -7,12 +7,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.persistence.DiscriminatorValue;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,6 +29,7 @@ class BoardControllerTest {
     void test() throws Exception {
         mockMvc.perform(get("/boards"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
+                .andExpect(content().string("Hello World"))
+                .andDo(print()); // Http 요청에 대한 요약을 출력해준다.
     }
 }
